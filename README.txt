110406cc35647db195c427c4ad1ef77c923f7eb4
CountDracula <pepe.lehtonen@gmail.com>
2016-12-13 18:24:57 +0100

    Added instructions for how to run program from cmdline

---
+++ README.txt
@@ -0,0 +1,11 @@
+To use the program:
+
+Step 1. Windows + R -> cmd
+Step 2. cd "path to the project", e.g. cd "C:\Workspace BU\InformationRetrieval-ADD"
+Step 3. javac ParseTester.java
+Step 4. java ParseTester
+
+Now you should be propmpted for the folder for files..
+Enter, eg. C:\temp\test
+
+Program outputs all required items.
--- src/ParseTester.java
+++ src/ParseTester.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by Pepe on 13.12.2016.
  */
 public class ParseTester {
 
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the folder where your files are located in: ");
+        String folderPath = scanner.next();
+        scanner.close();
+        System.out.println("Thank you. Proceeding with building the inverted index ...");
         IndexBuilder builder = new IndexBuilder();
 
-        builder.readFiles("C:\\temp\\test");
+
+        builder.readFiles(folderPath);
     }
 }
